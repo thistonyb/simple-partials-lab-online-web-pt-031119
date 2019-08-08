72d780ea43c38baae0712ef3ad5ba670f95437a7
Tony Baker <this.tonybaker@gmail.com>
2019-08-08 01:18:55 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 80.times do
-  Student.create(name: Faker::Name.name, hometown: Faker::Address.city, birthday: Faker::Date)
+  Student.create(name: Faker::Name.name, hometown: Faker::Address.city, birthday: Faker::Date.between(from: 25.years.ago, to: 18.years.ago))
 end
 Classroom.create(course_name: 'Math', semester: "Spring #{Time.now.year}")
 Classroom.create(course_name: 'English', semester: "Fall #{Time.now.year}")
